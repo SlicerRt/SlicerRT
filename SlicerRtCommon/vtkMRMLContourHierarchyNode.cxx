ee8fcbf7b5a0dd57d586ae7adf8ab212d00d26d1
pinter <pinter@25a7314f-c4c8-4314-9936-a25f8480a772>
2012-09-24 13:50:37 +0000

    Re #137: Added missing files
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/src@311 25a7314f-c4c8-4314-9936-a25f8480a772

---
+++ SlicerRtCommon/vtkMRMLContourHierarchyNode.cxx
@@ -0,0 +1,76 @@
+// MRML includes
+#include "vtkMRMLContourHierarchyNode.h"
+#include "vtkMRMLContourNode.h"
+#include "vtkMRMLScene.h"
+
+// VTK includes
+#include <vtkCollection.h>
+#include <vtkObjectFactory.h>
+
+//----------------------------------------------------------------------------
+vtkMRMLNodeNewMacro(vtkMRMLContourHierarchyNode);
+
+//----------------------------------------------------------------------------
+vtkMRMLContourHierarchyNode::vtkMRMLContourHierarchyNode()
+{
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLContourHierarchyNode::~vtkMRMLContourHierarchyNode()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLContourHierarchyNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  Superclass::PrintSelf(os,indent);
+}
+
+//----------------------------------------------------------------------------
+const char* vtkMRMLContourHierarchyNode::GetNodeTagName()
+{
+  return "ContourHierarchyNode";
+}
+//----------------------------------------------------------------------------
+void vtkMRMLContourHierarchyNode::ReadXMLAttributes( const char** atts)
+{
+  Superclass::ReadXMLAttributes(atts);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLContourHierarchyNode::WriteXML(ostream& of, int indent)
+{
+  Superclass::WriteXML(of,indent);
+}
+
+//---------------------------------------------------------------------------
+void vtkMRMLContourHierarchyNode::GetChildrenContourNodes(vtkCollection *contours)
+{
+  vtkMRMLScene *scene = this->GetScene();
+  if (!contours || !scene)
+    {
+    return;
+    }
+
+  vtkMRMLNode *mnode = NULL;
+  vtkMRMLContourHierarchyNode *hnode = NULL;
+  for (int n=0; n < scene->GetNumberOfNodes(); n++) 
+    {
+    mnode = scene->GetNthNode(n);
+    if (mnode->IsA("vtkMRMLContourNode"))
+      {
+      hnode = vtkMRMLContourHierarchyNode::SafeDownCast(
+        vtkMRMLDisplayableHierarchyNode::GetDisplayableHierarchyNode(scene, mnode->GetID()));
+
+      while (hnode)
+        {
+        if (hnode == this) 
+          {
+          contours->AddItem(mnode);
+          break;
+          }
+        hnode = vtkMRMLContourHierarchyNode::SafeDownCast(hnode->GetParentNode());
+        } // end while
+      } // end if
+    } // end for
+}
---
+++ SlicerRtCommon/vtkMRMLContourHierarchyNode.h
@@ -0,0 +1,41 @@
+// .NAME vtkMRMLContourHierarchyNode - MRML node to represent hierarchy of Contours.
+// .SECTION Description
+// n/a
+//
+
+#ifndef __vtkMRMLContourHierarchyNode_h
+#define __vtkMRMLContourHierarchyNode_h
+
+#include "vtkMRMLDisplayableHierarchyNode.h"
+
+/// \ingroup Slicer_QtModules_Contour
+class  vtkMRMLContourHierarchyNode : public vtkMRMLDisplayableHierarchyNode
+{
+public:
+  static vtkMRMLContourHierarchyNode *New();
+  vtkTypeMacro(vtkMRMLContourHierarchyNode,vtkMRMLDisplayableHierarchyNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  /// Read node attributes from XML file
+  virtual void ReadXMLAttributes( const char** atts);
+
+  /// Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  /// Get node XML tag name (like Volume, Contour)
+  virtual const char* GetNodeTagName();
+
+  /// Find all child model nodes in the hierarchy
+  virtual void GetChildrenContourNodes(vtkCollection *models);
+
+protected:
+  vtkMRMLContourHierarchyNode();
+  ~vtkMRMLContourHierarchyNode();
+  vtkMRMLContourHierarchyNode(const vtkMRMLContourHierarchyNode&);
+  void operator=(const vtkMRMLContourHierarchyNode&);
+
+};
+
+#endif
