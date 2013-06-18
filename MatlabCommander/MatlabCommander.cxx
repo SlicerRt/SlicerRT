0e45396f5310027c6a110341d75fecfecd56fcf1
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-06-18 15:43:32 +0000

    re #38: Added special mode to MatlabCommander that forwards all the command-line parameters in a cell to a specified function (if the first command-line argument is --call-matlab-function)
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/src@903 25a7314f-c4c8-4314-9936-a25f8480a772

--- MatlabCommander/MatlabCommander.cxx
+++ MatlabCommander/MatlabCommander.cxx
@@ -9,6 +9,16 @@
 #include "igtlStringMessage.h"
 #include "igtlClientSocket.h"
 
+const std::string CALL_MATLAB_FUNCTION_ARG="--call-matlab-function";
+const std::string MATLAB_DEFAULT_HOST="127.0.0.1";
+const int MATLAB_DEFAULT_PORT=4100;
+
+enum ExecuteMatlabCommandStatus
+{
+  COMMAND_STATUS_FAILED=0,
+  COMMAND_STATUS_SUCCESS=1
+};
+
 std::string ReceiveString(igtl::Socket * socket, igtl::MessageHeader::Pointer& header)
 {
   // Create a message buffer to receive transform data
@@ -43,31 +53,26 @@ void SetReturnValues(const std::string &returnParameterFile,const char* reply, b
   rts.close(); 
 }
 
-int main (int argc, char * argv [])
+ExecuteMatlabCommandStatus ExecuteMatlabCommand(const std::string& hostname, int port, const std::string &cmd, std::string &reply)
 {
-  PARSE_ARGS;
-  
-  std::cerr << "Executing Matlab command..." << std::endl;
-
   //------------------------------------------------------------
   // Establish Connection
   igtl::ClientSocket::Pointer socket;
   socket = igtl::ClientSocket::New();
   int connectErrorCode = socket->ConnectToServer(hostname.c_str(), port);
   if (connectErrorCode != 0)
-  {    
-    std::cerr << "Cannot connect to the server." << std::endl;
-    SetReturnValues(returnParameterFile,"ERROR: Cannot connect to the server",0);
-    exit(EXIT_SUCCESS);
+  {        
+    reply="ERROR: Cannot connect to the server";
+    return COMMAND_STATUS_FAILED;
   }
 
   //------------------------------------------------------------
   // Send command
   igtl::StringMessage::Pointer stringMsg;
   stringMsg = igtl::StringMessage::New();
-  stringMsg->SetDeviceName("MatlabCommandClient");
+  stringMsg->SetDeviceName("CMD");
   std::cout << "Sending string: " << cmd << std::endl;
-  stringMsg->SetString(cmd);
+  stringMsg->SetString(cmd.c_str());
   stringMsg->Pack();
   socket->Send(stringMsg->GetPackPointer(), stringMsg->GetPackSize());
 
@@ -82,37 +87,101 @@ int main (int argc, char * argv [])
   int receivedBytes = socket->Receive(headerMsg->GetPackPointer(), headerMsg->GetPackSize());
   if (receivedBytes == 0)
   {
-    std::cerr << "No reply" << std::endl;
+    reply="No reply";
     socket->CloseSocket();
-    SetReturnValues(returnParameterFile,"ERROR: No reply received",0);
-    exit(EXIT_SUCCESS);
+    return COMMAND_STATUS_FAILED;
   }
   if (receivedBytes != headerMsg->GetPackSize())
   {
-    std::cerr << "Bad reply" << std::endl;
+    reply = "Bad reply";
     socket->CloseSocket();
-    SetReturnValues(returnParameterFile,"ERROR: invalid reply received",0);
-    exit(EXIT_SUCCESS);
+    return COMMAND_STATUS_FAILED;
   }
   // Deserialize the header
   headerMsg->Unpack();
-  if (strcmp(headerMsg->GetDeviceType(), "STRING") == 0)
+  if (strcmp(headerMsg->GetDeviceType(), "STRING") != 0)
   {
-    reply=ReceiveString(socket, headerMsg);
-    std::cout << "Result: "<<reply<<std::endl;
-    SetReturnValues(returnParameterFile,reply.c_str(),1);
-  }
-  else
-  {
-    std::cerr << "Receiving unsupported message type: " << headerMsg->GetDeviceType() << std::endl;
+    reply = std::string("Receiving unsupported message type: ") + headerMsg->GetDeviceType();
     socket->Skip(headerMsg->GetBodySizeToRead(), 0);
-    SetReturnValues(returnParameterFile,"ERROR: invalid reply type received",0);
-    exit(EXIT_SUCCESS);
+    socket->CloseSocket();
+    return COMMAND_STATUS_FAILED;
   }
+  // Get the reply string
+  reply=ReceiveString(socket, headerMsg);
 
   //------------------------------------------------------------
   // Close connection
   socket->CloseSocket();
 
-  return EXIT_SUCCESS;
+  return COMMAND_STATUS_SUCCESS;
+}
+
+int CallMatlabFunction(int argc, char * argv [])
+{
+  std::string functionName=argv[2];
+  std::string cmd;
+  cmd+=functionName+"({";
+  for (int argvIndex=3; argvIndex<argc; argvIndex++)
+  {
+    std::string arg=argv[argvIndex];
+    if ( arg.at(0) == '"' ) 
+    {
+      // this is a quoted string => remove the quotes (Matlab uses different quotes anyway)
+      arg.erase( 0, 1 ); // erase the first character
+      arg.erase( arg.size() - 1 ); // erase the last character
+    }
+    cmd+=std::string("'")+arg+"'";
+    if (argvIndex+1<argc)
+    {
+      // not the last argument, so add a separator
+      cmd+=",";
+    }
+  }
+  cmd+="})";
+
+  std::cout << "Command: " << cmd << std::endl;
+
+  std::string reply;
+  ExecuteMatlabCommandStatus status=ExecuteMatlabCommand(MATLAB_DEFAULT_HOST, MATLAB_DEFAULT_PORT, cmd, reply);
+  if (status!=COMMAND_STATUS_SUCCESS)
+  {
+    std::cerr << "ERROR: " << reply << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << reply << std::endl;
+  return EXIT_SUCCESS;  
+}
+
+
+int CallStandardCli(int argc, char * argv [])
+{
+  PARSE_ARGS;
+  ExecuteMatlabCommandStatus status=ExecuteMatlabCommand(hostname, port, cmd, reply);
+  if (status==COMMAND_STATUS_SUCCESS)
+  {
+    std::cout << reply << std::endl;
+    completed=true;
+  }
+  else
+  {
+    std::cerr << "ERROR: " << reply << std::endl;
+    completed=false;
+  }
+  SetReturnValues(returnParameterFile,reply.c_str(),completed);    
+  return EXIT_SUCCESS; // always return with EXIT_SUCCESS, otherwise Slicer ignores the return values and we cannot show the reply on the module GUI
+}
+
+int main (int argc, char * argv [])
+{
+  if (argc>2 && CALL_MATLAB_FUNCTION_ARG.compare(argv[1])==0)
+  {
+    // MatlabCommander is called with arguments: --call-matlab-function function_name parameter1 parameter2 ...
+    return CallMatlabFunction(argc, argv);
+  }
+  else
+  {
+    // MatlabCommander is called as a standard CLI modul
+    return CallStandardCli(argc, argv);
+  }
 } 
