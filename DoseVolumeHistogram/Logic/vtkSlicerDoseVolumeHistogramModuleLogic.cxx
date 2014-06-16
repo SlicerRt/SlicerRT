fb54fc9b4b948ce94276b4e589e0dfa70ccf0482
rankin <rankin@25a7314f-c4c8-4314-9936-a25f8480a772>
2014-06-16 16:10:45 +0000

    re #446 adding node to scene to prevent NULL scene guard
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/src@1804 25a7314f-c4c8-4314-9936-a25f8480a772

--- DoseVolumeHistogram/Logic/vtkSlicerDoseVolumeHistogramModuleLogic.cxx
+++ DoseVolumeHistogram/Logic/vtkSlicerDoseVolumeHistogramModuleLogic.cxx
@@ -393,12 +393,13 @@ void vtkSlicerDoseVolumeHistogramModuleLogic::ComputeDvh(vtkMRMLContourNode* str
   // Get maximum dose from dose volume
   vtkNew<vtkImageAccumulate> doseStat;
   doseStat->SetInput(doseVolumeNode->GetImageData());
-  doseStat->Update();
+  doseStat-> Update();
   double maxDose = doseStat->GetMax()[0];
 
   // Get resampled dose volume and matching structure labelmap (the function makes sure their lattices are the same)
   vtkSmartPointer<vtkMRMLScalarVolumeNode> resampledDoseVolume = vtkSmartPointer<vtkMRMLScalarVolumeNode>::New();
   vtkSmartPointer<vtkMRMLContourNode> consolidatedStructureContourNode = vtkSmartPointer<vtkMRMLContourNode>::New();
+  this->GetMRMLScene()->AddNode(consolidatedStructureContourNode);
   this->GetOversampledDoseVolumeAndConsolidatedIndexedLabelmapForContour(structureContourNode, resampledDoseVolume, consolidatedStructureContourNode);
 
   // Create stencil for structure
