843b4d88785cc58cc8d59354c2d05383380ccdc6
maxime.desplanques <maxime.desplanques@25a7314f-c4c8-4314-9936-a25f8480a772>
2014-11-05 13:21:50 +0000

    Re #666 Error compilation std::not1 not found -> fixed
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/src@2008 25a7314f-c4c8-4314-9936-a25f8480a772

--- VffFileReader/Logic/vtkSlicerVffFileReaderLogic.cxx
+++ VffFileReader/Logic/vtkSlicerVffFileReaderLogic.cxx
@@ -43,6 +43,7 @@
 #include <string>
 #include <algorithm>
 #include <cctype>
+#include <functional>
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkSlicerVffFileReaderLogic);
