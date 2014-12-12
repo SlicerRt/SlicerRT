8e405308289ac55561ea811829f785dea85ba5bf
pinter <pinter@25a7314f-c4c8-4314-9936-a25f8480a772>
2014-12-12 17:39:14 +0000

    Re #54: BatchContourConversion works from command line. It is also a self-test module that downloads its test data automatically
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/src@2062 25a7314f-c4c8-4314-9936-a25f8480a772

--- BatchProcessing/BatchContourConversion.py
+++ BatchProcessing/BatchContourConversion.py
@@ -2,6 +2,8 @@ import os
 import unittest
 from __main__ import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
+import argparse
+import sys
 
 #
 # BatchContourConversion
@@ -12,7 +14,7 @@ class BatchContourConversion(ScriptedLoadableModule):
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
     parent.title = "Batch Contour Conversion"
-    parent.categories = ["Testing"]
+    parent.categories = ["Testing.SlicerRT Tests"]
     parent.dependencies = ["DicomRtImportExport", "Contours"]
     parent.contributors = ["Csaba Pinter (Queen's)"]
     parent.helpText = """
@@ -57,6 +59,92 @@ class BatchContourConversionLogic(ScriptedLoadableModuleLogic):
   def __init__(self):
     ScriptedLoadableModuleLogic.__init__(self)
 
+    self.dataDir = slicer.app.temporaryPath + '/BatchContourConversion'
+    if not os.access(self.dataDir, os.F_OK):
+      os.mkdir(self.dataDir)
+
+    self.dicomDatabaseDir = self.dataDir + '/CtkDicomDatabase'
+
+  def OpenDatabase(self):
+    # Open test database and empty it
+    if not os.access(self.dicomDatabaseDir, os.F_OK):
+      os.mkdir(self.dicomDatabaseDir)
+
+    dicomWidget = slicer.modules.dicom.widgetRepresentation().self()
+    dicomWidget.onDatabaseDirectoryChanged(self.dicomDatabaseDir)
+
+    slicer.dicomDatabase.initializeDatabase()
+
+  def ImportStudy(self,dicomDataDir):
+    indexer = ctk.ctkDICOMIndexer()
+
+    # Import study to database
+    indexer.addDirectory( slicer.dicomDatabase, dicomDataDir )
+    indexer.waitForImportFinished()
+
+  def LoadFirstPatientIntoSlicer(self):
+    # Choose first patient from the patient list
+    detailsPopup = slicer.modules.dicom.widgetRepresentation().self().detailsPopup
+    patient = slicer.dicomDatabase.patients()[0]
+    studies = slicer.dicomDatabase.studiesForPatient(patient)
+    series = [slicer.dicomDatabase.seriesForStudy(study) for study in studies]
+    seriesUIDs = [uid for uidList in series for uid in uidList]
+    detailsPopup.offerLoadables(seriesUIDs, 'SeriesUIDList')
+    detailsPopup.examineForLoading()
+
+    loadables = detailsPopup.loadableTable.loadables
+
+    # Load into Slicer
+    detailsPopup = slicer.modules.dicom.widgetRepresentation().self().detailsPopup
+    detailsPopup.loadCheckedLoadables()
+
+  def ConvertContoursToLabelmap(self):
+    import vtkSlicerContoursModuleLogic
+
+    labelmapsToSave = []
+    
+    # Get all contour nodes from the scene
+    contourNodes = slicer.mrmlScene.GetNodesByClass('vtkMRMLContourNode')
+    numberOfContours = contourNodes.GetNumberOfItems()
+
+    for contourIndex in xrange(0,numberOfContours):
+      contourNode = contourNodes.GetItemAsObject(contourIndex)
+      print('  Converting contour ' + contourNode.GetName())
+
+      # Set referenced volume as rasterization reference
+      referenceVolume = vtkSlicerContoursModuleLogic.vtkSlicerContoursModuleLogic.GetReferencedVolumeByDicomForContour(contourNode)
+      if referenceVolume == None:
+        import sys
+        sys.stderr.write('No reference volume found for contour ' + contourNode.GetName())
+        continue
+
+      contourNode.SetAndObserveRasterizationReferenceVolumeNodeId(referenceVolume.GetID())
+
+      # Perform conversion
+      contourNode.GetLabelmapImageData()
+      contourLabelmapNode = vtkSlicerContoursModuleLogic.vtkSlicerContoursModuleLogic.ExtractLabelmapFromContour(contourNode)
+
+      # Append contour to list
+      labelmapsToSave.append(contourLabelmapNode)
+      
+    return labelmapsToSave
+
+  def SaveLabelmaps(self,labelmapsToSave,outputDir):
+    for labelmapNode in labelmapsToSave:
+      # Assemble file name from patient name and contour name
+      fileName = labelmapNode.GetName() + '.nrrd'
+      filePath = outputDir + '/' + fileName
+      print('  Saving contour ' + labelmapNode.GetName() + '\n    to file ' + fileName)
+
+      # Workaround: create storage node to be able to save the labelmap
+      storageNode = labelmapNode.CreateDefaultStorageNode()
+      slicer.mrmlScene.AddNode(storageNode)
+      labelmapNode.SetAndObserveStorageNodeID(storageNode.GetID())
+
+      # Save to file
+      success = slicer.util.saveNode(labelmapNode, filePath)
+      if not success:
+        sys.stderr.write('Failed to save labelmap: ' + filePath)
 
 class BatchContourConversionTest(ScriptedLoadableModuleTest):
   """
@@ -70,47 +158,44 @@ class BatchContourConversionTest(ScriptedLoadableModuleTest):
 
     self.delayMs = 700
 
-    #TODO: Comment out
+    #TODO: Comment out - sample code for debugging by writing to file
     #logFile = open('d:/pyTestLog.txt', 'w')
     #logFile.write(repr(slicer.modules.batchcontourconversion) + '\n')
-    #logFile.write(repr(slicer.modules.dicomrtimportexport) + '\n')
     #logFile.close()
 
   def runTest(self):
     """Run as few or as many tests as needed here.
     """
     self.setUp()
+    
+    # Create logic
+    self.logic = BatchContourConversionLogic()
 
     self.test_BatchContourConversion_FullTest1()
 
   def test_BatchContourConversion_FullTest1(self):
-    # Check for DicomRtImportExport module
+    # Check for modules
     self.assertTrue( slicer.modules.dicomrtimportexport )
+    self.assertTrue( slicer.modules.contours )
 
     self.TestSection_0SetupPathsAndNames()
     self.TestSection_1RetrieveInputData()
     self.TestSection_2OpenDatabase()
     self.TestSection_3ImportStudy()
-    self.TestSection_4SelectLoadables()
-    self.TestSection_5LoadIntoSlicer()
-    self.TestSection_6ConvertContoursToLabelmap()
-    self.TestSection_7SaveLabelmaps()
+    self.TestSection_4LoadFirstPatientIntoSlicer()
+    self.TestSection_5ConvertContoursToLabelmap()
+    self.TestSection_6SaveLabelmaps()
     self.TestSection_0Clear()
 
 
   def TestSection_0SetupPathsAndNames(self):
-    self.dataDir = slicer.app.temporaryPath + '/BatchContourConversion_SelfTest'
-    if not os.access(self.dataDir, os.F_OK):
-      os.mkdir(self.dataDir)
-
-    self.dicomDataDir = self.dataDir + '/TinyRtStudy'
+    self.dicomDataDir = self.logic.dataDir + '/TinyRtStudy'
     if not os.access(self.dicomDataDir, os.F_OK):
       os.mkdir(self.dicomDataDir)
 
-    self.dicomDatabaseDir = self.dataDir + '/CtkDicomDatabase'
-    self.dicomZipFilePath = self.dataDir + '/TinyRtStudy.zip'
+    self.dicomZipFilePath = self.logic.dataDir + '/TinyRtStudy.zip'
     self.expectedNumOfFilesInDicomDataDir = 12
-    self.outputDir = self.dataDir + '/Output'
+    self.outputDir = self.logic.dataDir + '/Output'
     
   def TestSection_1RetrieveInputData(self):
     try:
@@ -134,7 +219,7 @@ class BatchContourConversionTest(ScriptedLoadableModuleTest):
 
       numOfFilesInDicomDataDir = len([name for name in os.listdir(self.dicomDataDir) if os.path.isfile(self.dicomDataDir + '/' + name)])
       if (numOfFilesInDicomDataDir != self.expectedNumOfFilesInDicomDataDir):
-        slicer.app.applicationLogic().Unzip(self.dicomZipFilePath, self.dataDir)
+        slicer.app.applicationLogic().Unzip(self.dicomZipFilePath, self.logic.dataDir)
         self.delayDisplay("Unzipping done",self.delayMs)
 
       numOfFilesInDicomDataDirTest = len([name for name in os.listdir(self.dicomDataDir) if os.path.isfile(self.dicomDataDir + '/' + name)])
@@ -147,107 +232,39 @@ class BatchContourConversionTest(ScriptedLoadableModuleTest):
 
   def TestSection_2OpenDatabase(self):
     self.delayDisplay("Open database",self.delayMs)
+    self.logic.OpenDatabase()
 
-    # Open test database and empty it
-    if not os.access(self.dicomDatabaseDir, os.F_OK):
-      os.mkdir(self.dicomDatabaseDir)
-
-    dicomWidget = slicer.modules.dicom.widgetRepresentation().self()
-    dicomWidget.onDatabaseDirectoryChanged(self.dicomDatabaseDir)
     self.assertTrue( slicer.dicomDatabase.isOpen )
-
-    initialized = slicer.dicomDatabase.initializeDatabase()
-    self.assertTrue( initialized )
 
   def TestSection_3ImportStudy(self):
     self.delayDisplay("Import study",self.delayMs)
-
-    indexer = ctk.ctkDICOMIndexer()
-    self.assertTrue( indexer )
-
-    # Import study to database
-    indexer.addDirectory( slicer.dicomDatabase, self.dicomDataDir )
-    indexer.waitForImportFinished()
+    self.logic.ImportStudy(self.dicomDataDir)
 
     self.assertTrue( len(slicer.dicomDatabase.patients()) > 0 )
     self.assertTrue( slicer.dicomDatabase.patients()[0] )
 
-  def TestSection_4SelectLoadables(self):
-    self.delayDisplay("Select loadables",self.delayMs)
+  def TestSection_4LoadFirstPatientIntoSlicer(self):
+    self.delayDisplay("Load first patient into Slicer",self.delayMs)
+    self.logic.LoadFirstPatientIntoSlicer()
 
-    # Choose first patient from the patient list
-    detailsPopup = slicer.modules.dicom.widgetRepresentation().self().detailsPopup
-    patient = slicer.dicomDatabase.patients()[0]
-    studies = slicer.dicomDatabase.studiesForPatient(patient)
-    series = [slicer.dicomDatabase.seriesForStudy(study) for study in studies]
-    seriesUIDs = [uid for uidList in series for uid in uidList]
-    detailsPopup.offerLoadables(seriesUIDs, 'SeriesUIDList')
-    detailsPopup.examineForLoading()
-
-    loadables = detailsPopup.loadableTable.loadables
-
-  def TestSection_5LoadIntoSlicer(self):
-    self.delayDisplay("Load into Slicer",self.delayMs)
-
-    detailsPopup = slicer.modules.dicom.widgetRepresentation().self().detailsPopup
-    detailsPopup.loadCheckedLoadables()
-
-  def TestSection_6ConvertContoursToLabelmap(self):
+  def TestSection_5ConvertContoursToLabelmap(self):
     self.delayDisplay("Convert loaded contours to labelmap",self.delayMs)
-
-    import vtkSlicerContoursModuleLogic
-
     qt.QApplication.setOverrideCursor(qt.QCursor(qt.Qt.BusyCursor))
-    self.labelmapsToSave = []
-    
-    # Get all contour nodes from the scene
-    contourNodes = slicer.mrmlScene.GetNodesByClass('vtkMRMLContourNode')
-    numberOfContours = contourNodes.GetNumberOfItems()
-
-    for contourIndex in xrange(0,numberOfContours):
-      contourNode = contourNodes.GetItemAsObject(contourIndex)
-      print('  Converting contour ' + contourNode.GetName())
-
-      # Set referenced volume as rasterization reference
-      referenceVolume = vtkSlicerContoursModuleLogic.vtkSlicerContoursModuleLogic.GetReferencedVolumeByDicomForContour(contourNode)
-      if referenceVolume == None:
-        import sys
-        sys.stderr.write('No reference volume found for contour ' + contourNode.GetName())
-        continue
-
-      contourNode.SetAndObserveRasterizationReferenceVolumeNodeId(referenceVolume.GetID())
-
-      # Perform conversion
-      contourNode.GetLabelmapImageData()
-      contourLabelmapNode = vtkSlicerContoursModuleLogic.vtkSlicerContoursModuleLogic.ExtractLabelmapFromContour(contourNode)
-      self.assertTrue(contourLabelmapNode != None)
-
-      # Append contour to list
-      self.labelmapsToSave.append(contourLabelmapNode)
-
+    try:
+      self.labelmapsToSave = self.logic.ConvertContoursToLabelmap()
+    except e, Exception:
+      import traceback
+      traceback.print_exc()
+      self.delayDisplay('Test caused exception!\n' + str(e),self.delayMs*2)
     qt.QApplication.restoreOverrideCursor()
 
-  def TestSection_7SaveLabelmaps(self):
+  def TestSection_6SaveLabelmaps(self):
     self.delayDisplay("Save labelmaps to directory\n  %s" % (self.outputDir),self.delayMs)
 
     self.assertTrue(len(self.labelmapsToSave) > 0)
     qt.QApplication.setOverrideCursor(qt.QCursor(qt.Qt.BusyCursor))
 
-    for labelmapNode in self.labelmapsToSave:
-      # Assemble file name from patient name and contour name
-      fileName = labelmapNode.GetName() + '.nrrd'
-      filePath = self.outputDir + '/' + fileName
-      print('  Saving contour ' + labelmapNode.GetName() + '\n    to file ' + fileName)
-
-      # Workaround: create storage node to be able to save the labelmap
-      storageNode = labelmapNode.CreateDefaultStorageNode()
-      slicer.mrmlScene.AddNode(storageNode)
-      labelmapNode.SetAndObserveStorageNodeID(storageNode.GetID())
-
-      # Save to file
-      success = slicer.util.saveNode(labelmapNode, filePath)
-      if not success:
-        sys.stderr.write('Failed to save labelmap: ' + filePath)
+    self.logic.SaveLabelmaps(self.labelmapsToSave,self.outputDir)
 
     self.delayDisplay('  Labelmaps saved to  %s' % (self.outputDir),self.delayMs)
     qt.QApplication.restoreOverrideCursor()
@@ -260,5 +277,51 @@ class BatchContourConversionTest(ScriptedLoadableModuleTest):
 
     slicer.dicomDatabase.closeDatabase()
     self.assertFalse( slicer.dicomDatabase.isOpen )
-    
+
     #slicer.mrmlScene.Clear(0) #TODO
+
+
+def main(argv):
+  try:
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Batch Contour Conversion")
+    parser.add_argument("-i", "--input-folder", dest="input_folder", metavar="PATH",
+                        default="-", required=True, help="Folder of input DICOM study")
+    parser.add_argument("-o", "--output-folder", dest="output_folder", metavar="PATH",
+                        default=".", help="Folder for output labelmaps")
+
+    args = parser.parse_args(argv)
+
+    # Check required arguments
+    if args.input_folder == "-":
+      print('Please specify input DICOM study folder!')
+    if args.output_folder == ".":
+      print('Current directory is selected as output folder (default). To change it, please specify --output-folder')
+
+    # Convert to python path style
+    input_folder = args.input_folder.replace('\\','/')
+    output_folder = args.output_folder.replace('\\','/')
+
+    # Perform batch conversion
+    logic = BatchContourConversionLogic()
+
+    print("Import DICOM data from " + input_folder)
+    logic.OpenDatabase()
+    logic.ImportStudy(input_folder)
+
+    print("Load first patient into Slicer")
+    logic.LoadFirstPatientIntoSlicer()
+
+    print("Convert loaded contours to labelmap volumes")
+    labelmaps = logic.ConvertContoursToLabelmap()
+
+    print("Save labelmaps to directory " + output_folder)
+    logic.SaveLabelmaps(labelmaps, output_folder)
+    print("DONE")
+
+  except Exception, e:
+    print e
+  sys.exit()
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
---
+++ BatchProcessing/_readme.txt
@@ -0,0 +1,3 @@
+BatchContourConversion
+  Convert all DICOM RTSS contours to labelmap and save them to disk
+  Usage: [path/]Slicer.exe --no-main-window --python-script [path/]BatchContourConversion.py --input-folder input/folder/path --output-folder output/folder/path (Optionally use -i and -o instead of the long argument names)
