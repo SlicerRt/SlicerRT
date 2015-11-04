df2010cddeff68b5db9f60df74cd9951124877b9
gregsharp <gregsharp@25a7314f-c4c8-4314-9936-a25f8480a772>
2015-11-04 18:59:39 +0000

    Test #778 Landwarp adds capability to save landmark registration vector field as MRML node
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/src@2486 25a7314f-c4c8-4314-9936-a25f8480a772

--- PlmLandwarp/plastimatch_slicer_landwarp.cxx
+++ PlmLandwarp/plastimatch_slicer_landwarp.cxx
@@ -139,11 +139,23 @@ main (int argc, char *argv[])
 
   do_landmark_warp (lw, plmslc_landwarp_rbf_type.c_str());
 
-  if (lw->m_warped_img && plmslc_landwarp_warped_volume != "None") {
+  if (lw->m_warped_img 
+    && plmslc_landwarp_warped_volume != ""
+    && plmslc_landwarp_warped_volume != "None") 
+  {
     lw->m_warped_img->save_image (plmslc_landwarp_warped_volume.c_str());
   }
-  if (lw->m_vf && plmslc_landwarp_vector_field != "None") {
-    xform_save (lw->m_vf, plmslc_landwarp_vector_field.c_str());
+  if (lw->m_vf 
+    && plmslc_landwarp_output_vf_f != ""
+    && plmslc_landwarp_output_vf_f != "None") 
+  {
+    xform_save (lw->m_vf, plmslc_landwarp_output_vf_f.c_str());
+  }
+  if (lw->m_vf 
+    && plmslc_landwarp_output_vf != ""
+    && plmslc_landwarp_output_vf != "None") 
+  {
+    xform_save (lw->m_vf, plmslc_landwarp_output_vf.c_str());
   }
 
   return EXIT_SUCCESS;
