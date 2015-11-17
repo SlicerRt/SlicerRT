bcde16ca919df66d0d14795ddd9f447d55bccd06
pinter <pinter@25a7314f-c4c8-4314-9936-a25f8480a772>
2015-11-17 15:38:28 +0000

    Re #580: Adding missing file from last commit
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/src@2499 25a7314f-c4c8-4314-9936-a25f8480a772

--- Isodose/Logic/vtkSlicerIsodoseModuleLogic.h
+++ Isodose/Logic/vtkSlicerIsodoseModuleLogic.h
@@ -48,8 +48,8 @@ public:
   static const char* ISODOSE_DEFAULT_ISODOSE_COLOR_TABLE_FILE_NAME;
   static const std::string ISODOSE_MODEL_NODE_NAME_PREFIX;
   static const std::string ISODOSE_PARAMETER_SET_BASE_NAME_PREFIX;
-  static const std::string ISODOSE_ISODOSE_SURFACES_HIERARCHY_NODE_NAME_POSTFIX;
-  static const std::string ISODOSE_ROOT_SUBJECT_HIERARCHY_NODE_NAME;
+  static const std::string ISODOSE_ROOT_MODEL_HIERARCHY_NODE_NAME_POSTFIX;
+  static const std::string ISODOSE_ROOT_SUBJECT_HIERARCHY_NODE_NAME_POSTFIX;
 
 public:
   static vtkSlicerIsodoseModuleLogic *New();
