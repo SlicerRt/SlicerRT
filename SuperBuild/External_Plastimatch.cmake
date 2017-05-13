1ef2ccf8c395f337f04962aa4b24c50659bf570d
pinter <pinter@25a7314f-c4c8-4314-9936-a25f8480a772>
2017-05-13 12:53:51 +0000

    Re #115, #822: Update Plastimatch in attempt to fix Mac build error
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/SlicerRt/src@3175 25a7314f-c4c8-4314-9936-a25f8480a772

--- SuperBuild/External_Plastimatch.cmake
+++ SuperBuild/External_Plastimatch.cmake
@@ -33,7 +33,7 @@ set (PLASTIMATCH_EXTRA_LIBRARIES
   -DPLMLIB_CONFIG_ENABLE_DOSE:BOOL=TRUE)
 
 # Choose which Plastimatch revision to build
-set (PLM_GIT_TAG "d077e84da943bdd292e115b6355e5808278557ed")
+set (PLM_GIT_TAG "e61d43074f306a987d021e485e377fd1325af173")
 
 # Figure out whether to use git or https
 if(NOT DEFINED git_protocol)
