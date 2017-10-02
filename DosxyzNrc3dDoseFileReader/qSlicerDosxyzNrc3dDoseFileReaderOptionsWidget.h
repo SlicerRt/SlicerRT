cc13814f1fdf02107fbb88818ad907733743223c
Anna Ilina <ilina.anna@queensu.ca>
2017-10-02 16:42:38 -0400

    ENH: Added DOSXYZnrc 3D dose file reader hidden module
    Includes option to set scaling factor for image intensity.
    
    https://github.com/SlicerRt/SlicerRT/issues/73

---
+++ DosxyzNrc3dDoseFileReader/qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget.h
@@ -0,0 +1,55 @@
+/*==============================================================================
+
+  Copyright (c) Laboratory for Percutaneous Surgery (PerkLab)
+  Queen's University, Kingston, ON, Canada. All Rights Reserved.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Anna Ilina, PerkLab, Queen's University
+  and was supported through the Applied Cancer Research Unit program of Cancer Care
+  Ontario.
+
+==============================================================================*/
+
+#ifndef __qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget_h
+#define __qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget_h
+
+// CTK includes
+#include <ctkPimpl.h>
+
+// SlicerQt includes
+#include "qSlicerIOOptionsWidget.h"
+
+// DosxyzNrc3dDoseFileReader includes
+#include "qSlicerDosxyzNrc3dDoseFileReaderModuleExport.h"
+
+class qSlicerDosxyzNrc3dDoseFileReaderOptionsWidgetPrivate;
+
+/// \ingroup SlicerRt_QtModules_DosxyzNrc3dDoseFileReader
+class Q_SLICER_DOSXYZNRC3DDOSEFILEREADER_EXPORT qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget :
+  public qSlicerIOOptionsWidget
+{
+  Q_OBJECT
+public:
+  typedef qSlicerIOOptionsWidget Superclass;
+  qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget(QWidget *parent=0);
+  virtual ~qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget();
+
+
+protected slots:
+  void updateProperties();
+
+private:
+  Q_DECLARE_PRIVATE_D(qGetPtrHelper(qSlicerIOOptions::d_ptr), qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget);
+  //Q_DECLARE_PRIVATE(qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget);
+  Q_DISABLE_COPY(qSlicerDosxyzNrc3dDoseFileReaderOptionsWidget);
+};
+
+#endif
---
+++ DosxyzNrc3dDoseFileReader/qSlicerDosxyzNrc3dDoseFileReaderPlugin.h
@@ -0,0 +1,60 @@
+/*==============================================================================
+
+  Copyright (c) Laboratory for Percutaneous Surgery (PerkLab)
+  Queen's University, Kingston, ON, Canada. All Rights Reserved.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Anna Ilina, PerkLab, Queen's University
+  and was supported through the Applied Cancer Research Unit program of Cancer Care
+  Ontario.
+
+==============================================================================*/
+
+#ifndef __qSlicerDosxyzNrc3dDoseFileReaderPlugin
+#define __qSlicerDosxyzNrc3dDoseFileReaderPlugin
+
+// SlicerQt includes
+#include "qSlicerFileReader.h"
+
+class qSlicerDosxyzNrc3dDoseFileReaderPluginPrivate;
+class vtkSlicerDosxyzNrc3dDoseFileReaderLogic;
+
+//-----------------------------------------------------------------------------
+/// \ingroup SlicerRt_QtModules_DosxyzNrc3dDoseFileReader
+class qSlicerDosxyzNrc3dDoseFileReaderPlugin
+  : public qSlicerFileReader
+{
+  Q_OBJECT
+
+public:
+  typedef qSlicerFileReader Superclass;
+  qSlicerDosxyzNrc3dDoseFileReaderPlugin(QObject* parent = 0);
+  qSlicerDosxyzNrc3dDoseFileReaderPlugin(vtkSlicerDosxyzNrc3dDoseFileReaderLogic* logic, QObject* parent = 0);
+  virtual ~qSlicerDosxyzNrc3dDoseFileReaderPlugin();
+
+  vtkSlicerDosxyzNrc3dDoseFileReaderLogic* logic()const;
+  void setLogic(vtkSlicerDosxyzNrc3dDoseFileReaderLogic* logic);
+
+  virtual QString description()const;
+  virtual IOFileType fileType()const;
+  virtual QStringList extensions()const;
+  virtual qSlicerIOOptions* options()const;
+  virtual bool load(const IOProperties& properties);
+
+protected:
+  QScopedPointer<qSlicerDosxyzNrc3dDoseFileReaderPluginPrivate> d_ptr;
+
+private:
+  Q_DECLARE_PRIVATE(qSlicerDosxyzNrc3dDoseFileReaderPlugin);
+  Q_DISABLE_COPY(qSlicerDosxyzNrc3dDoseFileReaderPlugin);
+};
+
+#endif
