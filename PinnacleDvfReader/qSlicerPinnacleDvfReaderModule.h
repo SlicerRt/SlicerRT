11c6da687edc00e342c949513e9d13f4dcb70cf9
Csaba Pinter <csaba.pinter@queensu.ca>
2017-11-21 14:30:37 -0500

    BUG: Added missing directives to experimental modules related to Qt5 migration

--- PinnacleDvfReader/qSlicerPinnacleDvfReaderModule.h
+++ PinnacleDvfReader/qSlicerPinnacleDvfReaderModule.h
@@ -35,6 +35,9 @@ class Q_SLICER_PINNACLEDVFREADER_EXPORT qSlicerPinnacleDvfReaderModule:
   public qSlicerLoadableModule
 {
   Q_OBJECT
+#ifdef Slicer_HAVE_QT5
+  Q_PLUGIN_METADATA(IID "org.slicer.modules.loadable.qSlicerLoadableModule/1.0");
+#endif
   Q_INTERFACES(qSlicerLoadableModule);
 
 public:
