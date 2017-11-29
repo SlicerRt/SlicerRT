4585458f9fcd858886d1bd1070a5ea929bad54b6
Csaba Pinter <csaba.pinter@queensu.ca>
2017-11-29 09:59:39 -0500

    BUG: Apply workaround to fix DICOM-RT export with VTK8
    Will need to be undone after SlicerRtCommon is properly wrapped by VTK8
    Re https://issues.slicer.org/view.php?id=4462

--- DicomRtImportExport/DicomRtImportExportPlugin.py
+++ DicomRtImportExport/DicomRtImportExportPlugin.py
@@ -76,7 +76,10 @@ class DicomRtImportExportPluginClass(DICOMPlugin):
     dataNode = shn.GetItemDataNode(subjectHierarchyItemID)
 
     # RT dose volume
-    if dataNode and vtkSlicerRtCommon.SlicerRtCommon.IsDoseVolumeNode(dataNode):
+    #if dataNode and vtkSlicerRtCommon.SlicerRtCommon.IsDoseVolumeNode(dataNode):
+    #TODO: Workaround until VTK8 can wrap SlicerRtCommon class https://issues.slicer.org/view.php?id=4462
+    if dataNode and dataNode.IsA('vtkMRMLScalarVolumeNode') \
+        and dataNode.GetAttribute('DicomRtImport.DoseVolume') is not None:
       exportable = slicer.qSlicerDICOMExportable()
       exportable.confidence = 1.0
       # Define type-specific required tags and default values
