e089c61f8f23a386694679d61e497b620da984ec
Csaba Pinter <pinter.csaba@gmail.com>
2020-03-19 11:20:35 +0000

    BUG: Workaround to fix crash when loading RT dose
    Need to figure out the underlying reason and fix the crash properly at its source.
    
    Re https://github.com/SlicerRt/SlicerRT/issues/135

--- DicomRtImportExport/Logic/vtkSlicerDicomRtImportExportModuleLogic.cxx
+++ DicomRtImportExport/Logic/vtkSlicerDicomRtImportExportModuleLogic.cxx
@@ -263,12 +263,13 @@ void vtkSlicerDicomRtImportExportModuleLogic::vtkInternal::ExamineRtDoseDataset(
   dicomDatabase->openDatabase(databaseFile, vtkSlicerDicomRtReader::DICOMREADER_DICOM_CONNECTION_NAME.c_str());
 
   // Get RTPlan name to show it with the dose
-  QString rtPlanLabelTag("300a,0002");
-  QString rtPlanFileName = dicomDatabase->fileForInstance(referencedSOPInstanceUID.c_str());
-  if (!rtPlanFileName.isEmpty())
-  {
-    name += OFString(": ") + OFString(dicomDatabase->fileValue(rtPlanFileName,rtPlanLabelTag).toUtf8().constData());
-  }
+  //TODO: Uncomment this line when figured out the reason for the crash, see https://github.com/SlicerRt/SlicerRT/issues/135
+  //QString rtPlanLabelTag("300a,0002");
+  //QString rtPlanFileName = dicomDatabase->fileForInstance(referencedSOPInstanceUID.c_str());
+  //if (!rtPlanFileName.isEmpty())
+  //{
+  //  name += OFString(": ") + OFString(dicomDatabase->fileValue(rtPlanFileName,rtPlanLabelTag).toUtf8().constData());
+  //}
 
   // Close and delete DICOM database
   dicomDatabase->closeDatabase();
