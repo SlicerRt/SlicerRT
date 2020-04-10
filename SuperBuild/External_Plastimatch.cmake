5921094d9d1f68bae6a36c3afaea5296ef310386
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2020-04-10 14:43:39 -0400

    BUG: Bump plastimatch version to fix problem with DICOM RTSTRUCT export

--- SuperBuild/External_Plastimatch.cmake
+++ SuperBuild/External_Plastimatch.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "b3797a65f6ca41c783214f6bfe0ae282fb42f797"
+    "2e729bc2c7c4eaa51184ed0acb23d91e615a7ff8"
     QUIET
     )
 
