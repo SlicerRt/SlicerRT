18d408b87ec328b9398168f2317700d897e9b73f
Niklas Wahl <n.wahl@dkfz-heidelberg.de>
2024-02-01 11:58:09 +0000

    Separate dose and dose influence matrix calculation in pyRadPlanEngine

--- ExternalBeamPlanning/Widgets/Python/pyRadPlanEngine.py
+++ ExternalBeamPlanning/Widgets/Python/pyRadPlanEngine.py
@@ -41,6 +41,7 @@ class pyRadPlanEngine(AbstractScriptedDoseEngine):
         self.scriptedEngine.addBeamParameterComboBox('pyRadPlan parameters','machine','machine','comment',['generic'],0)      
 
     def calculateDoseUsingEngine(self, beamNode, resultDoseVolumeNode):
+        
         # TODO: Avoid Hardcoded Information here
         os.chdir('D:/Python/pyRadPlan')
         sys.path.append('D:/Python/pyRadPlan')
@@ -190,6 +191,121 @@ class pyRadPlanEngine(AbstractScriptedDoseEngine):
         # Set name
         # DoseNodeName = str(beamNode.GetName())+"_pyRadDose"
         # resultDoseVolumeNode.SetName(DoseNodeName)
+        
+        return str() #return empty string to indicate success
+    
+    def calculateDoseInfluenceMatrixUsingEngine(self, beamNode):
+        # TODO: Avoid Hardcoded Information here
+        os.chdir('D:/Python/pyRadPlan')
+        sys.path.append('D:/Python/pyRadPlan')
+
+        import pyRadPlan.io.matRad as matRadIO
+        import pyRadPlan.matRad.engine as engine
+        #from pyRadPlan.GUI import matRadGUI
+        #from pyRadPlan.stf.generate_stf import generateStf
+        from pyRadPlan.dose.calcPhotonDose import calcPhotonDose, calcPhotonDoseMC
+        from pyRadPlan.dose.calcParticleDose import calcParticleDose, calcParticleDoseMC
+        # from pyRadPlan.optimization._fluenceOptimizer import FluenceOptimizer
+        from pyRadPlan.stf.StfGenerator_temporary import StfGenerator
+        from pyRadPlan.optimization._fluenceOptimizer import FluenceOptimizer
+        from pyRadPlan.patients._patient_loader import PatientLoader
+        from pymatreader import read_mat
+        from scipy.sparse import csr_matrix
+        import matplotlib.pyplot as plt
+
+        from pyRadPlan.optimization.components.objectives import (SquaredDeviation, SquaredOverdosing)
+
+        from pyRadPlan.optimization import fluenceOptimization
+        import pymedphys as pymed
+
+        # path2pyRadPlan = os.getcwd()
+        # temp_path = path2pyRadPlan + '/pyRadPlan/temp_files/'
+
+        parentPlanNode = beamNode.GetParentPlanNode()
+        # referenceVolumeNode = parentPlanNode.GetReferenceVolumeNode()        
+
+
+        '''
+        matRad needs to be installed on the PC, and the engine initialization has to point to it
+        choose between MatRadEngineOctave or MatRadEngineMatlab
+        '''
+        eng = engine.MatRadEngineMatlab('matRad')
+        engine.setEngine(eng)
+
+        # Prepare the ct
+        ct = self.prepareCt(beamNode)
+
+        # Prepare the cst (segmentations)
+        cst = self.prepareCst(beamNode)
+
+        # Prepare the plan configuration
+        pln = self.preparePln(beamNode)
+
+        # Generate the stf (inverse planning irradiation geommetry like beamlet positions, etc.)
+        '''
+        matRad functions ending in py which are part of the functions seen below have been slightly edited to allow for calling 
+        them with the matlab engine. A specific description of the changes can be found in the respective matRad functions.
+        '''
+        stf = StfGenerator(ct, cst, pln)
+
+        # An elaborate way to save all the dictionaries into structs that can be loaded by matlab. This will not be necessary
+        # once the dose calculation and optimization functions are set. It is there as a "place-holder" and a check if the
+        # stf generation works
+        # https://stackoverflow.com/questions/61542500/convert-multiple-python-dictionaries-to-matlab-structure-array-with-scipy-io-sav
+        matRadIO.save(self.temp_path, 'stf_with_separate_rays.mat', {'stf': np.array([stf[i] for i in range(len(stf))], dtype=object),
+                                                            'rays': np.array([[stf[j]['ray'][i] for i in range(len(stf[j]['ray']))]
+                                                                            for j in range(len(stf))], dtype=object)})#'ray':[stf[0]['ray'][i] for i in range(len(stf[0]['ray']))]})
+
+        # This is required to properly load all the structs that are inside stf_with_separate_rays.mat
+        eng.engine.IO_stf(self.temp_path)
+
+
+        # Calculate the photon dose using a matRad function called matRad_calcPhotonDose
+        # Only pencil beam implemented so far
+        if pln['radiationMode']=='photons':
+            calcPhotonDose('ct.mat', 'stf.mat', 'pln.mat', 'cst.mat', in_path=self.temp_path, out_path=self.temp_path)
+            #calcPhotonDoseMC('ct.mat', 'stf.mat', 'pln.mat', 'cst.mat', in_path=temp_path, out_path=temp_path)
+        elif pln['radiationMode']=='protons' or pln['radiationMode']=='carbon':
+            calcParticleDose('ct.mat', 'stf.mat', 'pln.mat', 'cst.mat', in_path=self.temp_path, out_path=self.temp_path)
+
+        # Setting up the plan configuration for the optimization algorithm. Only the RBE is necessary. We will get it from the
+        # previously defined pln dictionary
+
+        if pln['radiationMode'] == 'photons':
+            pln_optim = {'RBE': 1.0}
+        elif pln['radiationMode'] == 'protons':
+            pln_optim = {'RBE': 1.1}
+
+        ''' Preparing dose information for optimization '''
+
+        dose_path = os.path.join(self.temp_path,'dij.mat')
+        dij_mat = read_mat(dose_path)  # keeping read_mat here for now
+        dose_matrix = csr_matrix(dij_mat['dij']['physicalDose'])
+
+        dose_information = {
+            "resolution": {"x": 3.0,
+                        "y": 3.0,
+                        "z": 3.0},
+
+            'cubeDim': tuple(dij_mat['dij']['doseGrid']['dimensions']),
+
+            'numOfVoxels': np.prod(tuple(dij_mat['dij']['doseGrid']['dimensions'])),
+
+            'numOfFractions': pln['numOfFractions'],
+
+            'physicalDose': dose_matrix,  # dose influence matrix
+
+            'totalNumOfBixels': dose_matrix.shape[1]  # dof
+        }
+
+        for dimension in ('x', 'y', 'z'):
+            dose_information[dimension] = np.arange(
+                ct[dimension][0],
+                ct[dimension][-1]
+                + dose_information['resolution'][dimension],
+                dose_information['resolution'][dimension])
+            
+        return str() #return empty string to indicate success
 
     def prepareCt(self, beamNode):
         import pyRadPlan.io.matRad as matRadIO
