1d2341959cfe8265271955e367b0aad8f47d86b4
LiBu981 <lina.bucher@dkfz-heidelberg.de>
2025-08-26 10:39:47 +0200

    Update copyright

--- ExternalBeamPlanning/Widgets/qSlicerScriptedPlanOptimizer.cxx
+++ ExternalBeamPlanning/Widgets/qSlicerScriptedPlanOptimizer.cxx
@@ -2,8 +2,8 @@
 
   Program: 3D Slicer
 
-  Copyright (c) Laboratory for Percutaneous Surgery (PerkLab)
-  Queen's University, Kingston, ON, Canada. All Rights Reserved.
+  Copyright (c) German Cancer Research Center (DKFZ),
+  Heidelberg, Germany. All Rights Reserved.
 
   See COPYRIGHT.txt
   or http://www.slicer.org/copyright/copyright.txt for details.
@@ -14,7 +14,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 
-  This file was originally developed by Niklas Wahl, German Cancer Research Center (DKFZ)
+  This file was originally developed by Niklas Wahl, German Cancer Research Center (DKFZ).
 ==============================================================================*/
 
 // ExternalBeamPlanning includes
